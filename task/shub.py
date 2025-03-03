959d5dfb22e2400c5d912d19d8b8e7e5dd07126a
Shubham Kumar Sinha <s82007317@gmail.com>
2025-03-03 14:32:42 +0530

    Add files via upload

---
+++ task/shub.py
@@ -0,0 +1,53 @@
+print("QUESTION=1")
+a=10+5*2
+print(a)
+print("QUESTION=2")
+b=15
+b//=4
+print("Quotient=",b)
+print("QUESTION=3")
+c=23
+c%=5
+print("Remainder=",c)
+print("QUESTION=4")
+x=5
+x**=3
+print("Exponential=",x)
+print("QUESTION=5")
+y=8
+z=3
+print("Binary of y=",bin(y))
+print("Binary of z=",bin(z))
+print(y&z,"of and operator")
+print("QUESTION=6")
+d=12
+e=5
+print("Binary of d=",bin(d))
+print("Binary of e=",bin(e))
+print(y|z,"of or operator")
+print("QUESTION=7")
+f=10
+print("Right shift=",f>>2)
+print("QUESTION=8")
+g=7
+h=3
+print("Binary of g=",bin(g))
+print("Binary of h=",bin(h))
+print(g^h,"of xor operator")
+print("QUESTION=10")
+print(not(5>2 and 3<1))
+print("QUESTION=11")
+k=7
+k*=2+3
+print("k=",k)
+print("QUESTION=12")
+print(14%3+2*5)
+print("QUESTION=13")
+print(20 / 4 + 3 * 2 - 1)
+print("QUESTION=14")
+l=3
+m=9
+print(l < m and m % l == 0)
+
+
+
